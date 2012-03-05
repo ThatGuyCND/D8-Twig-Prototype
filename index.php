5f2149c49789f4c6ed592e8b9453717977ec171d
Mark Perkins <mark@allmarkedup.com>
2012-03-05 17:58:38 +0000

    Work on assets and tidying things up a bit

--- index.php
+++ index.php
@@ -12,26 +12,13 @@ try {
 } catch ( \Exception $e ) {
 	$error = $e->getMessage();
 	echo <<<EOD
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	
-	<link rel="stylesheet" href="/app/assets/css/bootstrap.min.css" type="text/css" media="screen">
-	<style type="text/css" media="screen">
-		body {
-			background: #EFEFEF;
-		}
-		
-		#pt-content {
-			padding: 15px 20px 1px 20px;
-			margin: 30px auto;
-			max-width: 500px;
-			background: #FFF;
-			border-radius: 8px;
-		}
-	</style>
+	<link rel="stylesheet" href="/app/assets/css/bootstrap.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/app/assets/css/system.css" type="text/css" media="all">
 
 	<title>An error has occurred.</title>
 </head>
@@ -42,10 +29,9 @@ try {
 			<h4 class="alert-heading">An error has occurred.</h4>
 			{$error}
 		</div>	
-	</div>	
+	</div>
 	
 </body>	
 </html>
-	
 EOD;
 }
