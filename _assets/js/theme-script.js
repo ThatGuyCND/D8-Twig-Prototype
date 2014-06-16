31ef9773a363f621f6d2723b9d94c33a10276f89
Erik Baldwin <ThatGuy@ErikBaldwin.com>
2014-06-15 21:28:21 -0700

    initializing with base scss and page templates

---
+++ _assets/js/theme-script.js
@@ -0,0 +1,18 @@
+(function ($) {
+  var w = window,
+      wH = $(w).height(),
+      wW = $(w).width(),
+      d = document,
+      dH = $(d).height(),
+      bgs = $('.background-elements'),
+      lGrps = bgs.find('.light--pair'),
+      l = bgs.find('.light');
+  
+  // w.addEventListener ("resize", function () {
+  //   var wH = $(w).height(),
+  //       wW = $(w).width();
+  // });
+  
+  // if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {} else {}
+
+})(jQuery);
