2586188372ead43d0fbbc834bc6829ca013bf123
Erik Baldwin <ThatGuy@ErikBaldwin.com>
2014-08-21 12:18:54 -0700

    removed unnecessary commented scripts from theme-script.js

--- _assets/js/theme-script.js
+++ _assets/js/theme-script.js
@@ -3,16 +3,6 @@
       wH = $(w).height(),
       wW = $(w).width(),
       d = document,
-      dH = $(d).height(),
-      bgs = $('.background-elements'),
-      lGrps = bgs.find('.light--pair'),
-      l = bgs.find('.light');
-  
-  // w.addEventListener ("resize", function () {
-  //   var wH = $(w).height(),
-  //       wW = $(w).width();
-  // });
-  
-  // if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {} else {}
+      dH = $(d).height();
 
 })(jQuery);
